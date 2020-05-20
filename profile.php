649fc34dcd3e91b0f31413ec0c7ea94c5af1b43e
truongdo789 <62229549+truongdo789@users.noreply.github.com>
2020-05-20 10:41:58 +0700

    abc

---
+++ profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>GREENWICH_Xedan</title>
+</head>
+<body>
+    <h1>Profile</h1>
+    <img src="https://cdn.bongdaplus.vn/Assets/Media/2018/01/02/62/anh-chu-anh-che-2-1.jpg" alt="" height="100px" width="50px">
+    <p><a href="https://www.facebook.com/chinhlano.vang">Hay den day voi toi</a></p>
+    <input type="text">
+    <?php
+        echo "Truong Do";
+    ?>
+</body>
+</html>
