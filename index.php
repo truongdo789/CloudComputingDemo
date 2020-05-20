e0187591104660ab78880277fee6cdcff883ed67
truongdo789 <62229549+truongdo789@users.noreply.github.com>
2020-05-20 10:12:44 +0700

    this changes to my app

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>FPT Greenwich</title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+
+    <?php
+        echo "Wednesday !!!@@@";
+        echo "Cloud computing";
+        echo "Change in fixbug branch";
+    ?>
+</body>
+</html>
